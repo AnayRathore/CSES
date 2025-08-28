6cb42cda0ec50e70936c105fff4ee9eaa6672028
Anay <anay090807@gmail.com>
2025-08-28 11:19:16 +0530

    weird

--- WeirdAlgo.cpp
+++ WeirdAlgo.cpp
@@ -1,11 +1,8 @@
-#include <bits/stdc++.h>
-
-using namespace std;
-
+#include <iostream>
 int main() {
-    int n;
-    cin>>n;
-    cout<<n<<" ";
+    long long n;
+    std::cin>>n;
+    std::cout<<n<<" ";
     while(n!=1){
         if(n&1){
             n=n*3+1;
@@ -13,7 +10,7 @@ int main() {
         else{
             n=n/2;
         }
-        cout<<n<<" ";
+        std::cout<<n<<" ";
     }
     return 0;
 }
